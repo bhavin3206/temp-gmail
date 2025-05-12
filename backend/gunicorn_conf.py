5369c9aa089f0df7775c72ad481bf43ba3003b94
bhavin3206 <bhavinpatel99987@gmail.com>
2025-05-12 20:07:25 +0530

    `Added logging configuration to gunicorn configuration file`

--- backend/gunicorn_conf.py
+++ backend/gunicorn_conf.py
@@ -5,3 +5,6 @@ bind = "127.0.0.1:8000"
 workers = (cpu_count() * 2) + 1
 worker_class = "uvicorn.workers.UvicornWorker"
 timeout = 120
+
+accesslog = "/var/log/temp_mail/access.log"
+errorlog = "/var/log/temp_mail/error.log"
